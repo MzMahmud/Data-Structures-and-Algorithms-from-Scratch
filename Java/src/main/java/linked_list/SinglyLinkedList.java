1cfde73fa3db315d1a06782f2d1483795c44e00e
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2022-06-29 16:39:57 +0600

    java singly linked list added

---
+++ Java/src/main/java/linked_list/SinglyLinkedList.java
@@ -0,0 +1,139 @@
+package linked_list;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SinglyLinkedList<T> implements Iterable<T> {
+
+    private Node<T> head, tail;
+    private int length;
+
+    public SinglyLinkedList() {
+        head = tail = null;
+        length = 0;
+    }
+
+    @SafeVarargs
+    public SinglyLinkedList(T... values) {
+        this();
+        Arrays.stream(values)
+              .sequential()
+              .forEach(this::pushBack);
+    }
+
+    public SinglyLinkedList(Collection<T> collection) {
+        this();
+        collection.forEach(this::pushBack);
+    }
+
+    public int size() {
+        return length;
+    }
+
+    public void pushFront(T value) {
+        head = new Node<>(value, head);
+        if (tail == null) {
+            tail = head;
+        }
+        ++length;
+    }
+
+    public void pushBack(T value) {
+        var newNode = new Node<>(value);
+        if (tail != null) {
+            tail.next = newNode;
+        } else {
+            head = newNode;
+        }
+        tail = newNode;
+        ++length;
+    }
+
+    public T popFront() throws NoSuchElementException {
+        if (head == null) {
+            throw listEmptyException;
+        }
+        T headData = head.data;
+        head = head.next;
+        --length;
+        if (length == 0) {
+            tail = head;
+        }
+        return headData;
+    }
+
+    public T peekHead() throws NoSuchElementException {
+        if (head == null) {
+            throw listEmptyException;
+        }
+        return head.data;
+    }
+
+    public T peekTail() throws NoSuchElementException {
+        if (tail == null) {
+            throw listEmptyException;
+        }
+        return tail.data;
+    }
+
+    public Stream<T> stream() {
+        return Stream.iterate(head, Objects::nonNull, current -> current.next)
+                     .map(Node::getData);
+    }
+
+    private String getString() {
+        return stream().map(Objects::toString)
+                       .collect(Collectors.joining(", ", "[", "]"));
+    }
+
+    @Override
+    public String toString() {
+        return getString();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new SinglyLinkedListIterator<>(head);
+    }
+
+    public static final class SinglyLinkedListIterator<T> implements Iterator<T> {
+        private Node<T> current;
+
+        public SinglyLinkedListIterator(Node<T> current) {
+            this.current = current;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public T next() {
+            var data = current.data;
+            current = current.next;
+            return data;
+        }
+    }
+
+    private static final NoSuchElementException listEmptyException = new NoSuchElementException("list is empty");
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        Node(T data) {
+            this(data, null);
+        }
+
+        T getData() {
+            return data;
+        }
+    }
+}
---
+++ Java/src/test/java/linked_list/SinglyLinkedListTest.java
@@ -0,0 +1,134 @@
+package linked_list;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class SinglyLinkedListTest {
+
+    @Test
+    void printEmptyList() {
+        var singlyLinkedList = new SinglyLinkedList<>();
+        System.out.println(singlyLinkedList);
+        System.out.printf("size = %d\n", singlyLinkedList.size());
+    }
+
+    @Test
+    void givenEmptyList_pushBackTest() {
+        var singlyLinkedList = new SinglyLinkedList<>();
+        singlyLinkedList.pushBack(1);
+        System.out.println(singlyLinkedList);
+        System.out.printf("size = %d\n", singlyLinkedList.size());
+    }
+
+    @Test
+    void givenEmptyList_pushFrontTest() {
+        var singlyLinkedList = new SinglyLinkedList<>();
+        singlyLinkedList.pushFront(1);
+        System.out.println(singlyLinkedList);
+        System.out.printf("size = %d\n", singlyLinkedList.size());
+    }
+
+    @Test
+    void givenEmptyList_pushFrontPushBackTest() {
+        var singlyLinkedList = new SinglyLinkedList<>();
+        singlyLinkedList.pushFront(1);
+        singlyLinkedList.pushBack(2);
+        singlyLinkedList.pushFront(3);
+        singlyLinkedList.pushFront(4);
+        singlyLinkedList.pushBack(5);
+        System.out.println(singlyLinkedList);
+        System.out.printf("size = %d\n", singlyLinkedList.size());
+    }
+
+    @Test
+    void constructorTest() {
+        System.out.println(new SinglyLinkedList<>());
+        System.out.println(new SinglyLinkedList<>(1));
+        System.out.println(new SinglyLinkedList<>(1, 2, 3));
+        System.out.println(new SinglyLinkedList<>(List.of(1, 2, 3)));
+    }
+
+    @Test
+    void streamTest() {
+        new SinglyLinkedList<>()
+                .stream()
+                .forEach(System.out::println);
+
+        new SinglyLinkedList<>(1, 2, 3)
+                .stream()
+                .map(n -> n * n)
+                .forEach(System.out::println);
+    }
+
+    @Test
+    void givenEmptyList_throwsNoSuchElementException() {
+        assertThrows(
+                NoSuchElementException.class,
+                () -> new SinglyLinkedList<>().peekHead()
+        );
+        assertThrows(
+                NoSuchElementException.class,
+                () -> new SinglyLinkedList<>().peekTail()
+        );
+        assertThrows(
+                NoSuchElementException.class,
+                () -> new SinglyLinkedList<>().popFront()
+        );
+    }
+
+    <T> void assertIsEmpty(SinglyLinkedList<T> singlyLinkedList) {
+        assertEquals(0, singlyLinkedList.size());
+        assertThrows(NoSuchElementException.class, new SinglyLinkedList<>()::peekHead);
+        assertThrows(NoSuchElementException.class, new SinglyLinkedList<>()::peekTail);
+        assertThrows(NoSuchElementException.class, new SinglyLinkedList<>()::popFront);
+    }
+
+    @Test
+    void givenSingleElementList_peekPopTest() {
+        var singlyLinkedList = new SinglyLinkedList<>(1);
+        assertEquals(1, singlyLinkedList.peekHead());
+        assertEquals(1, singlyLinkedList.peekTail());
+
+        assertEquals(1, singlyLinkedList.popFront());
+        assertIsEmpty(singlyLinkedList);
+    }
+
+    @Test
+    void givenMultiElementList_peekTest() {
+        var singlyLinkedList = new SinglyLinkedList<>(1, 2, 3);
+        assertEquals(1, singlyLinkedList.peekHead());
+        assertEquals(3, singlyLinkedList.peekTail());
+
+        System.out.println(singlyLinkedList.popFront());
+        System.out.printf("singlyLinkedList %s\nsize %d\n", singlyLinkedList, singlyLinkedList.size());
+        System.out.println(singlyLinkedList.popFront());
+        System.out.printf("singlyLinkedList %s\nsize %d\n", singlyLinkedList, singlyLinkedList.size());
+        System.out.println(singlyLinkedList.popFront());
+        System.out.printf("singlyLinkedList %s\nsize %d\n", singlyLinkedList, singlyLinkedList.size());
+
+        assertIsEmpty(singlyLinkedList);
+    }
+
+    @Test
+    void forEachLoopTest() {
+        System.out.println("empty list");
+        for (var number : new SinglyLinkedList<>()) {
+            System.out.println(number);
+        }
+
+        System.out.println("[1]");
+        for (var number : new SinglyLinkedList<>(1)) {
+            System.out.println(number);
+        }
+
+        System.out.println("[1,2,3]");
+        for (var number : new SinglyLinkedList<>(1, 2, 3)) {
+            System.out.println(number);
+        }
+    }
+}
